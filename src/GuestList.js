5fc186397d0d931af887c692a0d62ea7f1163825
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-05-11 21:43:01 +0200

    Add input styles and onClick

--- src/GuestList.js
+++ src/GuestList.js
@@ -10,21 +10,41 @@ export default function GuestList() {
 
   return (
     <>
-      <h1 className={styles.basicFlex}>HelloWorld</h1>
+      <h1 className={styles.basicFlex}>Guest List</h1>
       <form className={styles.basicBox}>
-        <label htmlFor="firstName">First Name</label>
-        <label htmlFor="lastName">Last Name</label>
-        <input id="firstName" value={firstName} placeholder="first name" />
-        <input
-          id="lastName"
-          value={lastName}
-          placeholder="last name"
-          onKeyDown={(event) => {
-            if (event.key === 'Enter') {
-              handleSubmit();
-            }
-          }}
-        />
+        <div className={styles.basicFlex}>
+          <label
+            htmlFor="firstName"
+            className={`${styles.structureBox} ${styles.subText}`}
+          >
+            First Name
+          </label>
+          <label
+            htmlFor="lastName"
+            className={`${styles.structureBox} ${styles.subText}`}
+          >
+            Last Name
+          </label>
+        </div>
+        <div className={styles.basicFlex}>
+          <input
+            id="firstName"
+            value={firstName}
+            placeholder="First name"
+            className={styles.structureBox}
+          />
+          <input
+            id="lastName"
+            value={lastName}
+            placeholder="Last name"
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                handleSubmit();
+              }
+            }}
+            className={styles.structureBox}
+          />
+        </div>
       </form>
       <div className={styles.basicBox}>Guest</div>
     </>
