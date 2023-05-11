8c5ad9a0a4f93de2c2cb044a1a6d742949e62cb0
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-05-11 20:46:59 +0200

    Import and set up states

--- src/GuestList.js
+++ src/GuestList.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import styles from './GuestList.module.scss';
 
 export default function GuestList() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const baseUrl = 'http://localhost:4000/';
+
+  function handleSubmit() {}
+
   return (
     <>
-      <h1>HelloWorld</h1>
-      <form className={styles.basicBox}>Form</form>
+      <h1 className={styles.basicFlex}>HelloWorld</h1>
+      <form className={styles.basicBox}>
+        <label htmlFor="firstName">First Name</label>
+        <label htmlFor="lastName">Last Name</label>
+        <input id="firstName" value={firstName} placeholder="first name" />
+        <input
+          id="lastName"
+          value={lastName}
+          placeholder="last name"
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              handleSubmit();
+            }
+          }}
+        />
+      </form>
       <div className={styles.basicBox}>Guest</div>
     </>
   );
