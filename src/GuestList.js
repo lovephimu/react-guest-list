c1ed2dbe8597392d7f1aa545394704c7a07a7db1
lovephimu <soe.nlb@gmail.com>
2023-05-11 16:29:58 +0200

    Initialize git

---
+++ src/GuestList.js
@@ -0,0 +1,11 @@
+import styles from './GuestList.module.scss';
+
+export default function GuestList() {
+  return (
+    <>
+      <h1>HelloWorld</h1>
+      <form>Form</form>
+      <div>Guest</div>
+    </>
+  );
+}
