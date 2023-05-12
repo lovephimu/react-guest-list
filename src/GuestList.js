1b66239768ab24174e9d305ec9f141be2ff01cbb
lovephimu <soe.nlb@gmail.com>
2023-05-12 19:04:11 +0200

    Add useEffect

--- src/GuestList.js
+++ src/GuestList.js
@@ -4,14 +4,43 @@ import styles from './GuestList.module.scss';
 export default function GuestList() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const baseUrl = 'http://localhost:4000/';
+  const [allGuests, setAllGuests] = useState([]);
 
   function handleSubmit() {}
+
+  // async function updateGuest() {
+  //   const response = await fetch('http://localhost:4000/guests/1', {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({ attending: true }),
+  //   });
+  // }
+
+  useEffect(() => {
+    async function getGuests() {
+      const response = await fetch('http://localhost:4000/guests');
+      console.log(response);
+      const data = await response.json();
+      const currentGuests = [...allGuests];
+      setAllGuests([...currentGuests, data[0]]);
+    }
+
+    getGuests().catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
   return (
     <>
       <h1 className={styles.basicFlex}>Guest List</h1>
-      <form className={styles.basicBox}>
+      <form
+        className={styles.basicBox}
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
         <div className={styles.basicFlex}>
           <label
             htmlFor="firstName"
@@ -46,7 +75,26 @@ export default function GuestList() {
           />
         </div>
       </form>
-      <div className={styles.basicBox}>Guest</div>
+      <section>
+        <div data-test-id="guest" className={styles.basicBox}>
+          <div
+            className={`${styles.basicFlex} ${styles.basicJustifyLeft} ${styles.basicAlignCenter} ${styles.basicGap}`}
+          >
+            <div>🤍</div>
+            <span className={styles.guestName}>
+              {allGuests[0].firstName} {allGuests[0].lastName}
+            </span>
+            <span>:</span>
+            <span>not attending</span>
+            <div
+              className={`${styles.basicFlex} ${styles.basicFlexItem} ${styles.basicJustifySelfFlexEnd} ${styles.basicAlignCenter} ${styles.basicGap}`}
+            >
+              <input type="checkbox" aria-label="attending" checked />
+              <button aria-label="Remove firstName lastName">Remove</button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
