8cf5fe5f5e13fc8a0c3be76dbf4c3283388960a9
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-05-14 20:51:17 +0200

    Add await loading

--- src/GuestListPrototype.js
+++ src/GuestListPrototype.js
@@ -12,6 +12,7 @@ export default function GuestList() {
     const response = await fetch('http://localhost:4000/guests');
     const data = await response.json();
     await setGuestListArray(data); // copying old data, pushing new fetched data and updating state in one go
+    await setLoading(false);
   }
 
   // first load useEffect
